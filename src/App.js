8a98f2d24cb427601a2e5a190305d18b963b2a05
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-04-05 01:10:51 +0600

    spinner and 404 done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reviews from './components/Reviews/Reviews'
 import About from './components/About/About'
 import Dashboard from './components/Dashboard/Dashboard'
 import Blogs from './components/Blogs/Blogs'
+import NotFound from './components/NotFound/NotFound'
 const App = () => {
   return (
     <div>
@@ -16,6 +17,7 @@ const App = () => {
         <Route path='/dashboard' element={<Dashboard />}></Route>
         <Route path='/blogs' element={<Blogs />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
 
       </Routes>
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Review from '../Review/Review';
-
+import Spinner from '../Spinner/Spinner'
 const Reviews = () => {
     const [reviews, setReviews] = useState([])
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
+        setLoading(true)
         fetch('reviews.json')
             .then(res => res.json())
-            .then(data => setReviews(data))
+            .then(data => {
+                setReviews(data)
+                setLoading(false)
+            }
+            )
+
     }, [])
     return (
-        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
-            <p className='text-center text-3xl font-bold text-gray'>What our Customer say!</p>
-            {/* <h1>length: {reviews.length}</h1> */}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
-                {
-                    reviews.map(review => <Review key={review.id} review={review}></Review>)
+        <>
+            {loading ? <Spinner /> : <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+                <p className='text-center text-3xl font-bold text-gray'>What our Customer say!</p>
+                {/* <h1>length: {reviews.length}</h1> */}
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center'>
+                    {
+                        reviews.map(review => <Review key={review.id} review={review}></Review>)
 
-                }
-            </div>
-        </div>
+                    }
+                </div>
+            </div>}
+
+        </>
     );
 };
 
